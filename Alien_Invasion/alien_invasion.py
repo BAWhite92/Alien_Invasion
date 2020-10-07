c6218efb2d821e2b9a064b9f30ef62a050a5c61f
Ben White <71787942+BAWhite92@users.noreply.github.com>
2020-10-07 17:03:32 +0100

    Add files via upload
    addition of settings within new file. minor changes in main file.

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("AlienInvasion")
 
+        #set the background colour
+        self.bg_colour = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -19,6 +22,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #redraw the screen during each pass through loop
+            self.screen.fill(self.bg_colour)
 
             #make most recent drawn screen visible.
             pygame.display.flip()
---
+++ Alien_Invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class for all settings within Alien Invasion game"""
+
+    def __init__(self):
+        """initialise game's settings"""
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
